f69003673cf14406c1e50902d0160129debc0875
GILLARD William <gillard@me.com>
2022-07-30 09:20:38 +0200

    "nitial commit

---
+++ test/gTest_version.h
@@ -0,0 +1,11 @@
+#ifndef _gTEST_version_
+#define _gTEST_version_
+
+#include<string> 
+
+std::string this_VERSION=std::string("v0.0.0-1-g2f65b7c");
+std::string this_VERSION_SHORT=std::string("v0.0.0");
+std::string this_VERSION_SHA1=std::string("1-g2f65b7c");
+std::string PROJECT_DIR=std::string("/Users/gillard/DeepSkyProject/deepskymath");
+
+#endif
