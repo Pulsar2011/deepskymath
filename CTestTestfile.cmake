a7a6829f9b5e906ef13519690b4dcf4cf76340d5
GILLARD William <gillard@cppm.in2p3.fr>
2022-07-30 09:27:32 +0200

    Delete unnecessary files

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gillard/DeepSkyProject/deepskymath
-# Build directory: /Users/gillard/DeepSkyProject/deepskymath
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/gillard/DeepSkyProject/deepskymath/gTest_version[1]_include.cmake")
-subdirs("googletest")
