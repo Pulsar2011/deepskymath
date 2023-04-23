be2dad3451b09f62ecd23fecebf8c70cd811c126
GILLARD William <gillard@cppm.in2p3.fr>
2023-04-23 09:25:17 +0200

    Improve test error message. Add more test.

--- test/gTest_MathCore.cxx
+++ test/gTest_MathCore.cxx
@@ -9,128 +9,187 @@ TEST(math_core_test, test_sign)
 
 #if __cplusplus >= 199711L
     ASSERT_EQ(sgn<uint16_t>((uint16_t) 125,std::is_signed<uint16_t>())==1,true)
-        <<"sgn((uint16_t)125) is positive ?!"<<sgn<uint16_t>((uint16_t) 125,std::is_signed<uint16_t>())<<std::endl;
+        <<"sgn((uint16_t)125) is positive ?!"<<sgn<uint16_t>((uint16_t) 125,std::is_signed<uint16_t>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<uint16_t>((uint16_t) 0,std::is_signed<uint16_t>())==0,true)
-        <<"sgn((uint16_t)125) is positive ?!"<<sgn<uint16_t>((uint16_t) 0,std::is_signed<uint16_t>())<<std::endl;
+        <<"sgn((uint16_t)125) is positive ?!"<<sgn<uint16_t>((uint16_t) 0,std::is_signed<uint16_t>())<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<uint32_t>((uint32_t) 125,std::is_signed<uint32_t>())==1,true)
-        <<"sgn((uint32_t)125) is positive ?!"<<sgn<uint32_t>((uint32_t) 125,std::is_signed<uint32_t>())<<std::endl;
+        <<"sgn((uint32_t)125) is positive ?!"<<sgn<uint32_t>((uint32_t) 125,std::is_signed<uint32_t>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<uint32_t>((uint32_t) 0,std::is_signed<uint32_t>())==0,true)
-        <<"sgn((uint32_t)125) is positive ?!"<<sgn<uint32_t>((uint32_t) 0,std::is_signed<uint32_t>())<<std::endl;
+        <<"sgn((uint32_t)125) is positive ?!"<<sgn<uint32_t>((uint32_t) 0,std::is_signed<uint32_t>())<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<uint64_t>((uint64_t) 125,std::is_signed<uint64_t>())==1,true)
-        <<"sgn((uint64_t)125) is positive ?!"<<sgn<uint64_t>((uint64_t) 125,std::is_signed<uint64_t>())<<std::endl;
+        <<"sgn((uint64_t)125) is positive ?!"<<sgn<uint64_t>((uint64_t) 125,std::is_signed<uint64_t>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<uint64_t>((uint64_t) 0,std::is_signed<uint64_t>())==0,true)
-        <<"sgn((uint64_t)125) is positive ?!"<<sgn<uint64_t>((uint64_t) 0,std::is_signed<uint64_t>())<<std::endl;
+        <<"sgn((uint64_t)125) is positive ?!"<<sgn<uint64_t>((uint64_t) 0,std::is_signed<uint64_t>())<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<int16_t>((int16_t) 125,std::is_signed<int16_t>())==1,true)
-        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) 125,std::is_signed<int16_t>())<<std::endl;
+        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) 125,std::is_signed<int16_t>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int16_t>((int16_t) 0,std::is_signed<int16_t>())==0,true)
-        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) 0,std::is_signed<int16_t>())<<std::endl;
+        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) 0,std::is_signed<int16_t>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int16_t>((int16_t) -125,std::is_signed<int16_t>())==-1,true)
-        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) -125,std::is_signed<int16_t>())<<std::endl;
+        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) -125,std::is_signed<int16_t>())<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<int32_t>((int32_t) 125,std::is_signed<int32_t>())==1,true)
-        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) 125,std::is_signed<int32_t>())<<std::endl;
+        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) 125,std::is_signed<int32_t>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int32_t>((int32_t) 0,std::is_signed<int32_t>())==0,true)
-        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) 0,std::is_signed<int32_t>())<<std::endl;
+        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) 0,std::is_signed<int32_t>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int32_t>((int32_t) -125,std::is_signed<int32_t>())==-1,true)
-        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) -125,std::is_signed<int32_t>())<<std::endl;
+        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) -125,std::is_signed<int32_t>())<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<int64_t>((int64_t) 125,std::is_signed<int64_t>())==1,true)
-        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) 125,std::is_signed<int64_t>())<<std::endl;
+        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) 125,std::is_signed<int64_t>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int64_t>((int64_t) 0,std::is_signed<int64_t>())==0,true)
-        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) 0,std::is_signed<int64_t>())<<std::endl;
+        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) 0,std::is_signed<int64_t>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int64_t>((int64_t) -125,std::is_signed<int64_t>())==-1,true)
-        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) -125,std::is_signed<int64_t>())<<std::endl;
+        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) -125,std::is_signed<int64_t>())<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<float>((float) 125,std::is_signed<float>())==1,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 125,std::is_signed<float>())<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 125,std::is_signed<float>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) 0,std::is_signed<float>())==0,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 0,std::is_signed<float>())<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 0,std::is_signed<float>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) -125,std::is_signed<float>())==-1,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) -125,std::is_signed<float>())<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) -125,std::is_signed<float>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) 1e-6,std::is_signed<float>())==1,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 1e-6,std::is_signed<float>())<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 1e-6,std::is_signed<float>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) 0,std::is_signed<float>())==0,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 0,std::is_signed<float>())<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 0,std::is_signed<float>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) -1e-6,std::is_signed<float>())==-1,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) -1e-6,std::is_signed<float>())<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) -1e-6,std::is_signed<float>())<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<double>((double) 125,std::is_signed<double>())==1,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 125,std::is_signed<double>())<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 125,std::is_signed<double>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) 0,std::is_signed<double>())==0,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 0,std::is_signed<double>())<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 0,std::is_signed<double>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) -125,std::is_signed<double>())==-1,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) -125,std::is_signed<double>())<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) -125,std::is_signed<double>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) 1e-6,std::is_signed<double>())==1,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 1e-6,std::is_signed<double>())<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 1e-6,std::is_signed<double>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) 0,std::is_signed<double>())==0,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 0,std::is_signed<double>())<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 0,std::is_signed<double>())<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) -1e-6,std::is_signed<double>())==-1,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) -1e-6,std::is_signed<float>())<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) -1e-6,std::is_signed<float>())<<" ["<<__LINE__<<"]"<<std::endl;
 
 #endif
 
     ASSERT_EQ(sgn<uint16_t>((uint16_t) 125)==1,true)
-        <<"sgn((uint16_t)125) is positive ?!"<<sgn<uint16_t>((uint16_t) 125)<<std::endl;
+        <<"sgn((uint16_t)125) is positive ?!"<<sgn<uint16_t>((uint16_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<uint16_t>((uint16_t) 0)==0,true)
-        <<"sgn((uint16_t)125) is positive ?!"<<sgn<uint16_t>((uint16_t) 0)<<std::endl;
+        <<"sgn((uint16_t)125) is positive ?!"<<sgn<uint16_t>((uint16_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((uint16_t) 125)==1,true)
+        <<"sgn((uint16_t)125) is positive ?!"<<sgn((uint16_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((uint16_t) 0)==0,true)
+        <<"sgn((uint16_t)125) is positive ?!"<<sgn((uint16_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<uint32_t>((uint32_t) 125)==1,true)
-        <<"sgn((uint32_t)125) is positive ?!"<<sgn<uint32_t>((uint32_t) 125)<<std::endl;
+        <<"sgn((uint32_t)125) is positive ?!"<<sgn<uint32_t>((uint32_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<uint32_t>((uint32_t) 0)==0,true)
-        <<"sgn((uint32_t)125) is positive ?!"<<sgn<uint32_t>((uint32_t) 0)<<std::endl;
+        <<"sgn((uint32_t)125) is positive ?!"<<sgn<uint32_t>((uint32_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((uint32_t) 125)==1,true)
+        <<"sgn((uint32_t)125) is positive ?!"<<sgn((uint32_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((uint32_t) 0)==0,true)
+        <<"sgn((uint32_t)125) is positive ?!"<<sgn((uint32_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<uint64_t>((uint64_t) 125)==1,true)
-        <<"sgn((uint64_t)125) is positive ?!"<<sgn<uint64_t>((uint64_t) 125)<<std::endl;
+        <<"sgn((uint64_t)125) is positive ?!"<<sgn<uint64_t>((uint64_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<uint64_t>((uint64_t) 0)==0,true)
-        <<"sgn((uint64_t)125) is positive ?!"<<sgn<uint64_t>((uint64_t) 0)<<std::endl;
+        <<"sgn((uint64_t)125) is positive ?!"<<sgn<uint64_t>((uint64_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((uint64_t) 125)==1,true)
+        <<"sgn((uint64_t)125) is positive ?!"<<sgn((uint64_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((uint64_t) 0)==0,true)
+        <<"sgn((uint64_t)125) is positive ?!"<<sgn((uint64_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<int16_t>((int16_t) 125)==1,true)
-        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) 125)<<std::endl;
+        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int16_t>((int16_t) 0)==0,true)
-        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) 0)<<std::endl;
+        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int16_t>((int16_t) -125)==-1,true)
-        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) -125)<<std::endl;
+        <<"sgn((int16_t)125) is positive ?!"<<sgn<int16_t>((int16_t) -125)<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<int32_t>((int32_t) 125)==1,true)
-        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) 125)<<std::endl;
+        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int32_t>((int32_t) 0)==0,true)
-        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) 0)<<std::endl;
+        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int32_t>((int32_t) -125)==-1,true)
-        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) -125)<<std::endl;
+        <<"sgn((int32_t)125) is positive ?!"<<sgn<int32_t>((int32_t) -125)<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<int64_t>((int64_t) 125)==1,true)
-        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) 125)<<std::endl;
+        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int64_t>((int64_t) 0)==0,true)
-        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) 0)<<std::endl;
+        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<int64_t>((int64_t) -125)==-1,true)
-        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) -125)<<std::endl;
+        <<"sgn((int64_t)125) is positive ?!"<<sgn<int64_t>((int64_t) -125)<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<float>((float) 125)==1,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 125)<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) 0)==0,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 0)<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 0)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) -125)==-1,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) -125)<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) -125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) 1e-6)==1,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 1e-6)<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 1e-6)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) 0)==0,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 0)<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) 0)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<float>((float) -1e-6)==-1,true)
-        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) -1e-6)<<std::endl;
+        <<"sgn((float)125) is positive ?!"<<sgn<float>((float) -1e-6)<<" ["<<__LINE__<<"]"<<std::endl;
 
     ASSERT_EQ(sgn<double>((double) 125)==1,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 125)<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) 0)==0,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 0)<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 0)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) -125)==-1,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) -125)<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) -125)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) 1e-6)==1,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 1e-6)<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 1e-6)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) 0)==0,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 0)<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) 0)<<" ["<<__LINE__<<"]"<<std::endl;
     ASSERT_EQ(sgn<double>((double) -1e-6)==-1,true)
-        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) -1e-6)<<std::endl;
+        <<"sgn((double)125) is positive ?!"<<sgn<double>((double) -1e-6)<<" ["<<__LINE__<<"]"<<std::endl;
+
+    ASSERT_EQ(sgn((int16_t) 125)==1,true)
+        <<"sgn((int16_t)125) is positive ?!"<<sgn((int16_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((int16_t) 0)==0,true)
+        <<"sgn((int16_t)125) is positive ?!"<<sgn((int16_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((int16_t) -125)==-1,true)
+        <<"sgn((int16_t)125) is positive ?!"<<sgn((int16_t) -125)<<" ["<<__LINE__<<"]"<<std::endl;
+
+    ASSERT_EQ(sgn((int32_t) 125)==1,true)
+        <<"sgn((int32_t)125) is positive ?!"<<sgn((int32_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((int32_t) 0)==0,true)
+        <<"sgn((int32_t)125) is positive ?!"<<sgn((int32_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((int32_t) -125)==-1,true)
+        <<"sgn((int32_t)125) is positive ?!"<<sgn((int32_t) -125)<<" ["<<__LINE__<<"]"<<std::endl;
+
+    ASSERT_EQ(sgn((int64_t) 125)==1,true)
+        <<"sgn((int64_t)125) is positive ?!"<<sgn((int64_t) 125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((int64_t) 0)==0,true)
+        <<"sgn((int64_t)125) is positive ?!"<<sgn((int64_t) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((int64_t) -125)==-1,true)
+        <<"sgn((int64_t)125) is positive ?!"<<sgn((int64_t) -125)<<" ["<<__LINE__<<"]"<<std::endl;
+
+    ASSERT_EQ(sgn((float) 125)==1,true)
+        <<"sgn((float)125) is positive ?!"<<sgn((float) 125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((float) 0)==0,true)
+        <<"sgn((float)125) is positive ?!"<<sgn((float) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((float) -125)==-1,true)
+        <<"sgn((float)125) is positive ?!"<<sgn((float) -125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((float) 1e-6)==1,true)
+        <<"sgn((float)125) is positive ?!"<<sgn((float) 1e-6)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((float) 0)==0,true)
+        <<"sgn((float)125) is positive ?!"<<sgn((float) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((float) -1e-6)==-1,true)
+        <<"sgn((float)125) is positive ?!"<<sgn((float) -1e-6)<<" ["<<__LINE__<<"]"<<std::endl;
+
+    ASSERT_EQ(sgn((double) 125)==1,true)
+        <<"sgn((double)125) is positive ?!"<<sgn((double) 125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((double) 0)==0,true)
+        <<"sgn((double)125) is positive ?!"<<sgn((double) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((double) -125)==-1,true)
+        <<"sgn((double)125) is positive ?!"<<sgn((double) -125)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((double) 1e-6)==1,true)
+        <<"sgn((double)125) is positive ?!"<<sgn((double) 1e-6)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((double) 0)==0,true)
+        <<"sgn((double)125) is positive ?!"<<sgn((double) 0)<<" ["<<__LINE__<<"]"<<std::endl;
+    ASSERT_EQ(sgn((double) -1e-6)==-1,true)
+        <<"sgn((double)125) is positive ?!"<<sgn((double) -1e-6)<<" ["<<__LINE__<<"]"<<std::endl;
 }
